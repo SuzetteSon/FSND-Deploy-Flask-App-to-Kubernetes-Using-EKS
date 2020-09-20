ed753c1e26812e4e7a3eae56d31266185667e2bf
Suzette <suzette.sonnekus@kaplan.se>
2020-09-20 16:38:41 +0200

    wip

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from flask import Flask, jsonify, request, abort
 
 JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
+TRUST = os.environ.get('TRUST')
 
 
 def _logger():
